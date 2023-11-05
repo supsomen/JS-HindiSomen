1faa872f54bbfa2e5f9450d577f8be8555589989
supsomen <61945725+supsomen@users.noreply.github.com>
2023-11-05 13:44:53 +0000

    datatype

--- .devcontainer/01_basics/02_Datatype.js
+++ .devcontainer/01_basics/02_Datatype.js
@@ -1,3 +1,26 @@
 "use strict"; //treat all JS cose as newer version
 
-alert(3+3)
+//alert(3+3) // w are using node js not browser
+
+console.log(3+3)
+console.log("Somen")
+
+let name="somen"
+let age=18
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean=> true and false
+//null=>standalone value
+//undefined=>
+//symbol=> 
+
+//object
+console.log(typeof null) // object
+
+
+
+
+
