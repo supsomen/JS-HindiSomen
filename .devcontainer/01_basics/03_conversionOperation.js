6145c0ea264e72dd4e3e8f8f45a608f08f89afa6
supsomen <61945725+supsomen@users.noreply.github.com>
2023-11-05 15:04:18 +0000

    conversion of data Type

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score="33"
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+// "33"=33
+//"33abc"=nan
+// true =>1; false=>0
+
+let isLoggedIn=1
+let boolenIsLoggedIn=Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn);
+
+// ""=>false
+//"somen"=true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+
